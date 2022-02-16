bd4e0758c326777c179444790f14e2f0927e6272
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-16 15:04:27 -0500

    api url change

--- src/components/helpers/environment.tsx
+++ src/components/helpers/environment.tsx
@@ -1,12 +1,12 @@
-let APIURL:string = '';
+let APIURL:string = 'https://mealprepmarket-server.herokuapp.com';
 
-switch (window.location.hostname) {
-  case 'localhost' || '127.0.0.1':
-    APIURL = 'https://mealprepmarket-server.herokuapp.com';
-    // APIURL = 'http://localhost:3001'
-    break;
-  case 'https://mealprepmarket-admin.herokuapp.com':
-    APIURL = 'https://mealprepmarket-server.herokuapp.com'
-}
+// switch (window.location.hostname) {
+//   case 'localhost' || '127.0.0.1':
+//     APIURL = 'https://mealprepmarket-server.herokuapp.com';
+//     // APIURL = 'http://localhost:3001'
+//     break;
+//   case 'https://mealprepmarket-admin.herokuapp.com':
+//     APIURL = 'https://mealprepmarket-server.herokuapp.com'
+// }
 
 export default APIURL;
